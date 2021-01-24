2e6b134c98b8564ee2cdb30164b64f21ba8588a8
Abhirup-99 <abhiruppalmethodist@gmail.com>
2021-01-24 20:26:20 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const allowedExt = [
+    '.js',
+    '.ico',
+    '.css',
+    '.png',
+    '.jpg',
+    '.woff2',
+    '.woff',
+    '.ttf',
+    '.svg',
+    '.gif',
+    '.GIF'
+];
+app.get('*', (req, res) => {
+    if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+        res.sendFile(path.join(__dirname, `${req.url}`));
+    } else {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    }
+});
+
+app.listen(3000)
